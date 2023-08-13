e2ae8c63550ee989eb2aeecae3385653f2e67337
Fermina Santurio <ferminasanturio@gmail.com>
2023-08-13 15:03:09 -0300

    Actualizando

--- libreria_virtual/mi_libreria/urls.py
+++ libreria_virtual/mi_libreria/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path("crear_genero/", views.crear_generos, name='crear_generos'),
     path("crear_autor/", views.crear_autores, name='crear_autores'),
     path("buscar_libro/", views.buscar_libros, name='buscar_libros'),
+    path('eliminar-libro/<int:id>/', views.eliminar_libro, name="eliminar_libro"),
+    path('eliminar-autor/<int:id>/', views.eliminar_autor, name="eliminar_autor"),
+    path('eliminar-genero/<int:id>/', views.eliminar_genero, name="eliminar_genero"),
+    path('editar-libro/<int:id>/', views.editar_libro, name="editar_libro"),
+    path('editar-autor/<int:id>/', views.editar_autor, name="editar_autor"),
+    path('editar-genero/<int:id>/', views.editar_genero, name="editar_genero"),
 ]
--- libreria_virtual/mi_libreria/views.py
+++ libreria_virtual/mi_libreria/views.py
@@ -131,5 +131,98 @@ def buscar_libros(request):
        )
        return http_response
 
+def eliminar_libro(request, id):
+   libro = Libro.objects.get(id=id)
+   if request.method == "POST":
+       libro.delete()
+       url_exitosa = reverse('lista_libros')
+       return redirect(url_exitosa)
+   
+def eliminar_autor(request, id):
+   autor = Autores.objects.get(id=id)
+   if request.method == "POST":
+       autor.delete()
+       url_exitosa = reverse('lista_autores')
+       return redirect(url_exitosa)
+   
+def eliminar_genero(request, id):
+   genero = Generos.objects.get(id=id)
+   if request.method == "POST":
+       genero.delete()
+       url_exitosa = reverse('lista_generos')
+       return redirect(url_exitosa)
+   
+def editar_libro(request, id):
+   libro = Libro.objects.get(id=id)
+   if request.method == "POST":
+       formulario = LibroFormulario(request.POST)
+
+       if formulario.is_valid():
+           data = formulario.cleaned_data
+           libro.nombre = data['nombre']
+           libro.autor = data['autor']
+           libro.save()
+           url_exitosa = reverse('lista_libros')
+           return redirect(url_exitosa)
+   else:  # GET
+       inicial = {
+           'nombre': libro.nombre,
+           'autor': libro.autor,
+       }
+       formulario = LibroFormulario(initial=inicial)
+   return render(
+       request=request,
+       template_name='mi_libreria/crear_libros.html',
+       context={'formulario': formulario},
+   )
+
+def editar_autor(request, id):
+   autor = Autores.objects.get(id=id)
+   if request.method == "POST":
+       formulario = AutorFormulario(request.POST)
+
+       if formulario.is_valid():
+           data = formulario.cleaned_data
+           autor.nombre = data['nombre']
+           autor.apellido = data['apellido']
+           autor.save()
+           url_exitosa = reverse('lista_autores')
+           return redirect(url_exitosa)
+   else:  # GET
+       inicial = {
+           'nombre': autor.nombre,
+           'apellido': autor.apellido,
+       }
+       formulario = AutorFormulario(initial=inicial)
+   return render(
+       request=request,
+       template_name='mi_libreria/crear_autores.html',
+       context={'formulario': formulario},
+   )
+
+def editar_genero(request, id):
+   genero = Generos.objects.get(id=id)
+   if request.method == "POST":
+       formulario = GeneroFormulario(request.POST)
+
+       if formulario.is_valid():
+           data = formulario.cleaned_data
+           genero.nombre = data['nombre']
+           genero.sub_genero = data['sub_genero']
+           genero.save()
+           url_exitosa = reverse('lista_generos')
+           return redirect(url_exitosa)
+   else:  # GET
+       inicial = {
+           'nombre': genero.nombre,
+           'sub_genero': genero.sub_genero,
+       }
+       formulario = GeneroFormulario(initial=inicial)
+   return render(
+       request=request,
+       template_name='mi_libreria/crear_generos.html',
+       context={'formulario': formulario},
+   )
+
 
 
