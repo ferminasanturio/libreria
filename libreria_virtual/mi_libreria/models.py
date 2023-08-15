31220f3cbecabab400b8e67531347bf0399465b7
Fermina Santurio <ferminasanturio@gmail.com>
2023-08-15 20:55:11 -0300

    Actualizo

--- libreria_virtual/mi_libreria/models.py
+++ libreria_virtual/mi_libreria/models.py
@@ -16,6 +16,7 @@ class Libro(models.Model):
 class Generos(models.Model):
     nombre = models.CharField(max_length=32)
     sub_genero = models.CharField(max_length=128)
+    creador = models.ForeignKey(User, default=1, null=True, on_delete=models.CASCADE, related_name='generos_creados')
 
     def __str__(self):
         return f'{self.nombre}, {self.sub_genero}'
@@ -25,6 +26,7 @@ class Autores(models.Model):
     nombre = models.CharField(max_length=256)
     fecha_nacimiento = models.DateField(null=True, blank=True)
     bio = models.TextField(blank=True)
+    creador = models.ForeignKey(User, default=1, null=True, on_delete=models.CASCADE, related_name='autores_creados')
 
     def __str__(self):
         return f'{self.apellido}, {self.nombre}'
