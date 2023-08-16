cef2a94256910a9fa2a452324dc63c4fa88f53ea
Fermina Santurio <ferminasanturio@gmail.com>
2023-08-15 22:43:10 -0300

    Cambiando

--- libreria_virtual/mi_libreria/urls.py
+++ libreria_virtual/mi_libreria/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.inicio, name="inicio"),
+    path("", views.inicio, name='inicio'),
     path("libros/", views.listar_libros, name='lista_libros'),
     path("generos/", views.listar_generos, name='lista_generos'),
     path("autores/", views.listar_autores, name='lista_autores'),
