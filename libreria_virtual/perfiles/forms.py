7ee097ce278afaf9c2408970fbbf352aea9d6102
Fermina Santurio <ferminasanturio@gmail.com>
2023-08-16 23:59:52 -0300

    Guardando

--- libreria_virtual/perfiles/forms.py
+++ libreria_virtual/perfiles/forms.py
@@ -1,15 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from perfiles.models import Avatar
 
 class UserRegisterForm(UserCreationForm):
    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
    password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
 
-   class Meta:
-    model = User
-    fields = ['last_name', 'first_name', 'username', 'email', 'password1', 'password2']
+class Meta:
+   model = User
+   fields = ['last_name', 'first_name', 'username', 'email', 'password1', 'password2']
 class UserUpdateForm(forms.ModelForm):
-   class Meta:
-    model = User
-    fields = ['last_name', 'first_name', 'email']
+    class Meta:
+      model = User
+      fields = ['last_name', 'first_name', 'email']
+
+class AvatarFormulario(forms.ModelForm):
+
+     class Meta:
+         model = Avatar
+         fields = ['imagen']
