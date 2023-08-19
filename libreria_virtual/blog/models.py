ba0cdb09fae34d187b4feeba1088b0d8d59bef93
Fermina Santurio <ferminasanturio@gmail.com>
2023-08-19 20:12:33 -0300

    Actualizo

--- libreria_virtual/blog/models.py
+++ libreria_virtual/blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Articulo(models.Model):
-    titulo = models.CharField(max_length=64)
-    subtitulo = models.CharField(max_length=64)
+    titulo = models.CharField(max_length=256)
+    subtitulo = models.CharField(max_length=256)
     cuerpo = models.TextField()
     autor = models.CharField(max_length=64)
     fecha = models.DateField()
